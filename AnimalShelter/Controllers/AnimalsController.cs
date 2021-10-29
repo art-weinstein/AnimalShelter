8b41a1cf4b5c2467482303faeb5e91040bb870d6
Art Weinstein <artur.weinstein@gmail.com>
2021-10-29 12:03:02 -0700

    update AnimalsController and add full CRUD functionality to the API

--- AnimalShelter/Controllers/AnimalsController.cs
+++ AnimalShelter/Controllers/AnimalsController.cs
@@ -21,19 +21,19 @@ namespace AnimalShelter.Controllers
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<Animal>>> Get(string animalName, string animalType)
+    public async Task<ActionResult<IEnumerable<Animal>>> Get()
     {
       var query = _db.Animals.AsQueryable();
 
-      if (animalName != null)
-      {
-        query = query.Where(entry => entry.AnimalName == animalName);
-      }
+      // if (animalName != null)
+      // {
+      //   query = query.Where(entry => entry.AnimalName == animalName);
+      // }
 
-      if (animalType != null)
-      {
-        query = query.Where(entry => entry.AnimalType == animalType);
-      }
+      // if (animalType != null)
+      // {
+      //   query = query.Where(entry => entry.AnimalType == animalType);
+      // }
       return await query.ToListAsync();
     }
 
@@ -48,6 +48,61 @@ namespace AnimalShelter.Controllers
         return NotFound();
       }
       return animal;
+    }
+
+    [HttpPut("{id}")]
+    
+    public async Task<ActionResult> Put(int id, Animal animal)
+    {
+      if(id != animal.AnimalId)
+      {
+        return BadRequest();
+      }
+      _db.Entry(animal).State = EntityState.Modified;
+
+      try
+      {
+        await _db.SaveChangesAsync();
+      }
+      catch (DbUpdateConcurrencyException)
+      {
+        if (!AnimalExists(id))
+        {
+          return NotFound();
+        }
+        else
+        {
+          throw;
+        }
+      }
+      return NoContent();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Animal>> Post(Animal animal)
+    {
+      _db.Animals.Add(animal);
+      await _db.SaveChangesAsync();
+      return CreatedAtAction(nameof(GetAnimal), new{id = animal.AnimalId}, animal);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteAnimal(int id)
+    {
+      var animal = await _db.Animals.FindAsync(id);
+      if (animal == null)
+      {
+        return NotFound();
+      }
+
+      _db.Animals.Remove(animal);
+      await _db.SaveChangesAsync();
+      return NoContent();
+    }
+
+    private bool AnimalExists(int id)
+    {
+      return _db.Animals.Any(e => e.AnimalId == id);
     }
   }
 }
